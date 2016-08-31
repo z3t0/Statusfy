11c084d50b328d8e23395468f419e44cc908ef21
Rafi Khan <zetoslab@gmail.com>
2016-08-30 18:24:19 -0600

    Initial Commit

---
+++ statusfy/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  statusfy
+//
+//  Created by Rafi Khan on 2016-08-30.
+//  Copyright © 2016 Rafi Khan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
